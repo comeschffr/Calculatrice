124cb8359c8b2171bb96a1a460c010a50f514811
comeschffr <48532306+comeschffr@users.noreply.github.com>
2019-05-24 18:27:28 +0200

    Update calculatrice.py

--- calculatrice.py
+++ calculatrice.py
@@ -2,43 +2,43 @@ import tkinter as tk
 
 
 class Calculatrice:
-    def __init__(self, screenSize, widgNbColumn):
+    def __init__(self, winWidth, winHeight, screenSize, widgNbColumn):
         
+        self.winWidth = winWidth
+        self.winHeight = winHeight
         self.screenSize = screenSize
         self.widgNbColumn = widgNbColumn
-        self.widthSquare = 400 / self.widgNbColumn
-        self.heightSquare = (650 - 150) / self.widgNbColumn
+        self.widthKey = self.winWidth / self.widgNbColumn # Single key width
+        self.heightKey = (self.winHeight - self.screenSize) / self.widgNbColumn # Single key height
         self.expression = '' # str type for calculation
 
         self.root = tk.Tk() # Initialize window
         self.root.title('Calculatrice')
-        self.root.geometry("400x650")
+        self.root.geometry("{}x{}".format(winWidth, winHeight))
 
         self.root.bind('<Button-1>', self.clic) # Gets coords when clic
         self.root.bind('<Motion>', self.motion) # To change colors when moving mouse
 
-        self.can = tk.Canvas(self.root, width=400, height=650, bg="white")
+        self.can = tk.Canvas(self.root, width=self.winWidth, height=self.winHeight, bg="white")
         self.can.pack()
-                                                        # "#%02x%02x%02x", (58, 64, 69)
-        self.can.create_rectangle(0, 0, 400, 150, fill='#282828') #screen
-        self.pad = self.can.create_rectangle(0, 150, 400, 650, fill='#191919') #pad
+
+        self.can.create_rectangle(0, 0, self.winWidth, self.screenSize, fill='#282828') #screen
+        self.pad = self.can.create_rectangle(0, self.screenSize, self.winWidth, self.winHeight, fill='#191919') #pad
 
         self.onScreen = self.can.create_text(370, 75, text=self.expression, anchor='e', fill='white', font='size 60') # Disp calculation on screen
 
-        line1 = (' ' * 3) + '7' + (' ' * 7) + '8' + (' ' * 7) + '9' + (' ' * 7) + '*'
-        line2 = (' ' * 3) + '4' + (' ' * 7) + '5' + (' ' * 7) + '6' + (' ' * 7) + '/'
-        line3 = (' ' * 3) + '1' + (' ' * 7) + '2' + (' ' * 7) + '3' + (' ' * 7) + '+'
-        line4 = (' ' * 3) + '0' + (' ' * 7) + ' . ' + (' ' * 7) + '=' + (' ' * 7) + '-'
+        lines = [(' ' * 3) + '7' + (' ' * 7) + '8' + (' ' * 7) + '9' + (' ' * 7) + '*',
+            (' ' * 3) + '4' + (' ' * 7) + '5' + (' ' * 7) + '6' + (' ' * 7) + '/',
+            (' ' * 3) + '1' + (' ' * 7) + '2' + (' ' * 7) + '3' + (' ' * 7) + '+',
+            (' ' * 3) + '0' + (' ' * 7) + ' . ' + (' ' * 7) + '=' + (' ' * 7) + '-']
 
-        self.can.create_text(0, 225, text=line1, anchor='w', fill='white', font='size 50')
-        self.can.create_text(0, 350, text=line2, anchor='w', fill='white', font='size 50')
-        self.can.create_text(0, 475, text=line3, anchor='w', fill='white', font='size 50')
-        self.can.create_text(0, 600, text=line4, anchor='w', fill='white', font='size 50')
+        for i in range(4):
+            self.can.create_text(0, (self.screenSize / 2 + (i + 1) * (self.heightKey)), text=lines[i], anchor='w', fill='white', font='size 50')
 
         self.root.mainloop()
 
 
-    def motion(event):
+    def motion(self, event):
         pass
 
 
@@ -46,8 +46,8 @@ class Calculatrice:
         clavier = [[0, '.', '=', '-'], [1, 2, 3, '+'], [4, 5, 6, '/'], [7, 8, 9, '*']]
 
         self.x, self.y = event.x, event.y
-        ligne = self.widgNbColumn - 1 - int((self.y - self.screenSize) / self.heightSquare)
-        colonne = int(self.x / self.widthSquare)
+        ligne = self.widgNbColumn - 1 - int((self.y - self.screenSize) / self.heightKey)
+        colonne = int(self.x / self.widthKey)
         
         element = str(clavier[ligne][colonne])
         
@@ -75,4 +75,4 @@ class Calculatrice:
             self.expression = ''
 
 
-calc1 = Calculatrice(150, 4)
+calc1 = Calculatrice(400, 650, 150, 4)
